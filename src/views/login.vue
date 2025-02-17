4adb77e2b8de8ad39b2bbe3ef8754cbc9641039b
IN5TAKILL538 <galviscristian538@gmail.com>
2025-02-17 10:12:43 -0500

    front login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,11 +1,33 @@
 <template>
-   <div>
-    <h1>pagina1</h1>
+   <div class="containerLogin">
+      <div class="image">
+         <h3>Iniciar Sesion</h3>
+        </div>
+       <div class="form">
+        
+           
+           
+           <!--boton email-->
+           <input v-model="email" placeholder="Email address" class="input" name="text" type="email" />
+           
+           <!--boton password-->
+           <input v-model="password" class="input" type="password" placeholder="password">
+       
+          <!--  <button class="button" @click="iniciarSesion">
+               
+           Iniciar Sesion
+           </button> -->
+           <q-btn id="btnLoading" :loading="loading" color="secondary" @click="iniciarSesion()" label="Iniciar sesion"  />
+       </div>
    </div>
+   
+     
+   </template>
+<script>
 
 
-</template>
-<script></script>
+
+</script>
 <style scoped>
 @import "../styles/login.css";
 </style>
